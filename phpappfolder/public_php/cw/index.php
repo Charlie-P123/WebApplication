d10de02be8bb486c84f6b1a6e79979a9cfff33a9
charl <P2439374@my365.dmu.ac.uk>
2021-01-18 02:04:06 +0000

    changes

--- phpappfolder/public_php/cw/index.php
+++ phpappfolder/public_php/cw/index.php
@@ -1,21 +1,2 @@
 <?php
-include_once 'cw/bootstrap.php';
-
-$use_xdebug = true;
-
-if ($use_xdebug && function_exists(xdebug_start_trace()))
-{
-    ini_set('display_errors', 'On');
-    ini_set('html_errors', 'On');
-    ini_set('xdebug.trace_output_name', 'sessions.t%');
-    ini_set('xdebug.trace_format', 1);
-
-    xdebug_start_trace();
-}
-
-include_once 'session/bootstrap.php';
-
-if ($use_xdebug && function_exists(xdebug_stop_trace()))
-{
-    xdebug_stop_trace();
-}
+include_once 'phpappfolder/includes/ce/bootstrap.php';

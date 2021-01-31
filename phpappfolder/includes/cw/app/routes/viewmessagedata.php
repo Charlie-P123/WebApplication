5e5d7664ca91b4f85192fbda4ead948eb6c3731a
charl <P2439374@my365.dmu.ac.uk>
2021-01-31 23:56:11 +0000

    routes.php

--- phpappfolder/includes/cw/app/routes/viewmessagedata.php
+++ phpappfolder/includes/cw/app/routes/viewmessagedata.php
@@ -2,7 +2,6 @@
 
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
-
 $app->get('/viewmessagedata', function(Request $request, Response $response) use ($app)
 {
     $view_message = getMessageData($app);

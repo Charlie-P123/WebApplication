8c657820dfd5e8cd7a565eca032094eb103f6d65
Declan-Work <p2420459@my365.dmu.ac.uk>
2021-02-10 03:02:22 +0000

    sqlqueries.php

--- phpappfolder/includes/cw/app/src/SQLQueries.php
+++ phpappfolder/includes/cw/app/src/SQLQueries.php
@@ -1,7 +1,34 @@
 <?php
 
+namespace cw;
 
 class SQLQueries
 {
+    public function __construct() {}
+    public function __destruct() {}
 
+    public function getMessageData()
+    {
+        $query_string = "SELECT board_id, msisdn, name, date, switch1, switch2,";
+        $query_string .= " switch3, switch4, fan, temperature, keypad";
+        $query_string .= " FROM board_status";
+        return $query_string;
+    }
+    public function insertMessageData()
+    {
+        $query_string = "INSERT INTO board_status";
+        $query_string .= " SET ";
+        $query_string .= "msisdn = :msisdn, ";
+        $query_string .= "name = :team_name, ";
+        $query_string .= "date = :timestamp, ";
+        $query_string .= "switch1 = :switch1, ";
+        $query_string .= "switch2 = :switch2, ";
+        $query_string .= "switch3 = :switch3, ";
+        $query_string .= "switch4 = :switch4, ";
+        $query_string .= "fan = :fan_state, ";
+        $query_string .= "temperature = :heater_temp, ";
+        $query_string .= "keypad = :keypad";
+
+        return $query_string;
+    }
 }
